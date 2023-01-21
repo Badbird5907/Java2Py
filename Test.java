efeb42ed05775bf6898b631f480b0f654e8d6a44
Badbird5907 <50347938+Badbird5907@users.noreply.github.com>
2023-01-20 21:38:30 -0500

    I did some stuff

---
+++ Test.java
@@ -0,0 +1,25 @@
+public class Test {
+    private static final String STRING = "Hello, World!";
+    private static final int INT = 42;
+    private static final long LONG = 42L;
+    private static final float FLOAT = 42.0f;
+    private static final double DOUBLE = 42.0;
+    private static final boolean BOOLEAN = true;
+    private static final byte BYTE = 42;
+    private static final short SHORT = 42;
+    private static final char CHAR = 'A';
+
+    private String string = "Hello, World!";
+    private int integer = 42;
+    private long longInteger = 42L;
+    private float floatNumber = 42.0f;
+    private double doubleNumber = 42.0;
+    private boolean booleanValue = true;
+    private byte byteValue = 42;
+    private short shortValue = 42;
+    private char charValue = 'A';
+
+    public static void main(String[] args) {
+
+    }
+}
--- src/main/java/dev/badbird/java2py/Java2Py.java
+++ src/main/java/dev/badbird/java2py/Java2Py.java
@@ -1,16 +1,73 @@
 package dev.badbird.java2py;
 
+import org.objectweb.asm.*;
+
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.util.Arrays;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 public class Java2Py {
     private static final Map<String, String> TYPES_MAP;
+
     static {
         TYPES_MAP = new HashMap<>();
         TYPES_MAP.put("java.lang.String", "str");
         TYPES_MAP.put("java.lang.Integer", "i32");
-    }
-    public Java2Py(byte[] classBytes) {
+        TYPES_MAP.put("java.lang.Long", "i64");
+        TYPES_MAP.put("java.lang.Float", "f32");
+        TYPES_MAP.put("java.lang.Double", "f64");
+        TYPES_MAP.put("java.lang.Boolean", "bool");
+        TYPES_MAP.put("java.lang.Byte", "u8");
+        TYPES_MAP.put("java.lang.Short", "i16");
+        TYPES_MAP.put("java.lang.Character", "char");
 
+        // primitive types
+        TYPES_MAP.put("int", "i32");
+        TYPES_MAP.put("long", "i64");
+        TYPES_MAP.put("float", "f32");
+        TYPES_MAP.put("double", "f64");
+        TYPES_MAP.put("boolean", "bool");
+        TYPES_MAP.put("byte", "u8");
+        TYPES_MAP.put("short", "i16");
+        TYPES_MAP.put("char", "char");
+    }
+
+    private ClassReader classReader;
+
+    public Java2Py(byte[] classBytes) {
+        classReader = new ClassReader(classBytes);
+
+        classReader.accept(new ClassVisitor(Opcodes.ASM7) {
+            @Override
+            public FieldVisitor visitField(int access, String name, String desc,
+                                           String signature, Object cst) {
+                System.out.println("access: " + access + " name: " + name + " desc: " + desc + " signature: " + signature + " cst: " + cst);
+                return super.visitField(access, name, desc, signature, cst);
+            }
+
+            @Override
+            public MethodVisitor visitMethod(int access, String name, String desc,
+                                             String signature, String[] exceptions) {
+                MethodVisitor mv = super.visitMethod(
+                        access, name, desc, signature, exceptions);
+                return new MethodVisitor(Opcodes.ASM5, mv) {
+                    @Override
+                    public void visitLdcInsn(Object cst) {
+                        //System.out.println("cst: " + cst + " access: " + access + " name: " + name + " desc: " + desc + " signature: " + signature + " exceptions: " + Arrays.toString(exceptions));
+                        super.visitLdcInsn(cst);
+                    }
+                };
+            }
+        }, 0);
+    }
+
+    public static void main(String[] args) throws IOException {
+        File file = new File("Test.class");
+        byte[] classBytes = Files.readAllBytes(file.toPath());
+        Java2Py java2Py = new Java2Py(classBytes);
     }
 }
